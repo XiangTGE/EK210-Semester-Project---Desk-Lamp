be6a9de2f9f30bac667ab5bda002f0f9e2374394
XiangTGE <xiang01nij@gmail.com>
2024-02-28 17:24:35 -0500

    a

--- VoiceRecognitionModuleTest/VoiceRecognitionModuleTest.ino
+++ VoiceRecognitionModuleTest/VoiceRecognitionModuleTest.ino
@@ -1,3 +1,11 @@
+// EK210 Semester Project 
+// Designer: Xiang Jin
+
+/*
+ * Used to test the feasibility of using a voice recognition module to control brightness of a desk lamp
+ * Inspired by clapper technology
+ */
+
 void setup() {
   // put your setup code here, to run once:
 
