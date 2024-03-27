c024e2e43d561835b9168b48035eb0828f3faec4
alexmw123 <163942312+alexmw123@users.noreply.github.com>
2024-03-27 16:25:14 -0400

    Update Lamp_Control_Software.ino
    Edit void loop to check for orientation command for each of the 4 quadrants. Also changed the void led_on_off to be compatible with the orientation commands.

--- Lamp_Control_Software/Lamp_Control_Software.ino
+++ Lamp_Control_Software/Lamp_Control_Software.ino
@@ -19,13 +19,12 @@
  *  - Orientation: Has four section in LED array that can be turned on independently of each other
  *
  * On/off commands:
- *  - "Lights on" (records 0 to 2)
- *  - "Lights off" (records 3 to 5)
+ *  - "Lamp" (records 0 to 2)
  * 
  * Brightness commands:
- *  - "Low" - 33% brightness (records 6 to 8)
- *  - "Medium" - 66% brightness (records 9 to 11)
- *  - "High" - 100% brightness (records 12 to 14)
+ *  - "Low" - 33% brightness (records 3 to 5)
+ *  - "Medium" - 66% brightness (records 6 to 8)
+ *  - "High" - 100% brightness (records 9 to 11)
  *
  * Timer Aspect:
  *  - User will have option to turn on/off the timer, which sets lamp to turn off by itself after a set period of time
@@ -33,7 +32,10 @@
  *  - If lamp is off, it will turn on again after motion sensor is triggered
  *
  * Orientation Commands:
- *  - 
+ *  - Quadrant I - records 12 to 14
+ *  - Quadrant II - records 15 to 17
+ *  - Quadrant III - records 18 to 20
+ *  - Quadrant IV - records 21 to 23
 */
 
 #include <SoftwareSerial.h>
@@ -184,28 +186,43 @@ void loop()
   // Check if voice command received
   if(ret>0){
 
-    if (buf[1] >= 0 && buf[1] <= 3) {           // Check for on command
-
-      // Turn on all controllable segments
-      led_on_off("on");
-    } else if (buf[1] >= 4 && buf[1] <= 7) {    // Check for off command
+    if (buf[1] >= 0 && buf[1] <= 3) {           // Check for on/off command
 
       // Turn off all controllable segments
-      led_on_off("off");
-    } else if (buf[1] >= 8 && buf[1] <= 23) {   // Check for brightness command
+      led_on_off("onoff");
+    } else if (buf[1] >= 3 && buf[1] <= 11) {   // Check for brightness command
 
       // Swtiches brightness based on input
-      if (buf[1] >= 8 && buf[1] <= 11)
+      if (buf[1] >= 3 && buf[1] <= 5)
         brightness_control(1);
 
-      else if (buf[1] >= 12 && buf[1] <= 15)
+      else if (buf[1] >= 6 && buf[1] <= 8)
         brightness_control(2);
 
-      else if (buf[1] >= 16 && buf[1] <= 19)
+      else if (buf[1] >= 9 && buf[1] <= 11)
         brightness_control(3);
         
-    } else if (buf[1] >= 24 && buf[1] <= 39) {  // Check for orientation command
-
+    } else if (buf[1] >= 12 && buf[1] <= 23) {  // Check for orientation command
+        if (buf[1] >= 12 && buf[1] <= 14)
+          if activeLED[1] == 1
+            activeLED[1] = 1;
+          else 
+            activeLED[1] = 0;
+        else if (buf[1] >= 15 && buf[1] <= 17)
+          if activeLED[2] == 1
+            activeLED[2] = 1;
+          else 
+            activeLED[2] = 0;
+        else if (buf[1] >= 18 && buf[1] <= 20)
+          if activeLED[3] == 1
+            activeLED[3] = 1;
+          else 
+            activeLED[3] = 0;
+        else if (buf[1] >= 21 && buf[1] <= 23)
+          if activeLED[4] == 1
+            activeLED[4] = 1;
+          else 
+            activeLED[4] = 0;
 
     } else {
 
@@ -218,19 +235,19 @@ void loop()
 }
 
 
-// Toggle controllable LED segments on/off
-// "toggle" can be "on" or "off"
-void led_on_off (String toggle) {
+
+// Controllable LED segments on/off
+void led_on_off () {
 
   int numLEDs = sizeof(activeLED) / sizeof(int);
 
   for (int i = 0; i < numLEDs; i++) {
 
-    if (activeLED[i] && toggle == "on")
-      digitalWrite(led[i], HIGH);
-    
-    else if (activeLED[i] && toggle == "off")
+    if (activeLED[i] == 0)
       digitalWrite(led[i], LOW);
+    
+    else if (activeLED[i] == 1)
+      digitalWrite(led[i], HIGH);
   }
 }
 
